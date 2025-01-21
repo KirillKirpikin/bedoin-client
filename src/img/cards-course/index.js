f7a1be51ef58fe3419fd46c38d5772bcefdf3e9f
KirillKirpikin <kirpikin25@gmail.com>
2025-01-21 11:20:33 +0200

    added img

--- src/img/cards-course/index.js
+++ src/img/cards-course/index.js
@@ -2,5 +2,6 @@ import Card_1 from "./course_card_1.jpg";
 import Card_2 from "./course_card_2.jpg";
 import Card_3 from "./course_card_3.jpg";
 import Card_4 from "./course_card_4.jpg";
+import Card_5 from "./course_card_5.jpg";
 
-export { Card_1, Card_2, Card_3, Card_4 };
+export { Card_1, Card_2, Card_3, Card_4, Card_5 };
